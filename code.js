9f575b82cb344159c99be43a5e6e2800d9f14616
Christian Giordano <christian@nuthinking.com>
2019-11-13 08:53:02 +0100

    added color parsers

--- code.js
+++ code.js
@@ -11,6 +11,39 @@ class CustomPaint {
         this.color = rgb;
     }
 }
+class Variable {
+}
+const parseRGBAValue = (rawValue) => {
+    const regex = /\([0-9][^\)]*\)/;
+    const match = rawValue.match(regex);
+    if (match) {
+        let value = match[0];
+        value = value.substr(1);
+        value = value.substring(0, value.length - 1);
+        const values = value.split(',');
+        const r = parseFloat(values[0].trim()) / 255;
+        const g = parseFloat(values[1].trim()) / 255;
+        const b = parseFloat(values[2].trim()) / 255;
+        const a = parseFloat(values[3].trim());
+        return {
+            r, g, b, a
+        };
+    }
+    console.error(`Couldn't parse RGBA value ${rawValue}`);
+    return { r: 0, b: 0, g: 0, a: 0 };
+};
+const parseHexValue = (rawValue) => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(rawValue);
+    if (result) {
+        const r = parseInt(result[1], 16) / 255;
+        const g = parseInt(result[2], 16) / 255;
+        const b = parseInt(result[3], 16) / 255;
+        const a = 1;
+        return { r, g, b, a };
+    }
+    console.error(`Couldn't parse HEX value ${rawValue}`);
+    return { r: 0, b: 0, g: 0, a: 0 };
+};
 const parseStyles = (content) => {
     // remove comments
     const commentsReg = /\/\*[\s\S]*?\*\/|\/\/.*/g;
@@ -20,11 +53,58 @@ const parseStyles = (content) => {
     // remove empty lines
     lines = lines.filter(line => line.trim().length > 0);
     console.log(`will parse ${lines.length} lines`);
+    // save variables with raw value, overriding
+    let map = {};
     lines.forEach(line => {
-        console.log(line);
+        line = line.trim();
+        const comp = line.split(':');
+        const name = comp[0].trim();
+        let rawValue = comp[1].trim();
+        // remove ':'
+        rawValue = rawValue.substring(0, rawValue.length - 1);
+        const variable = new Variable();
+        variable.name = name;
+        variable.rawValue = rawValue;
+        map[name] = variable;
     });
-    // save variables with value, overriding
+    const getVariableValue = (rawValue) => {
+        if (rawValue.startsWith('rgba(')) {
+            return parseRGBAValue(rawValue);
+        }
+        if (rawValue.startsWith('#')) {
+            return parseHexValue(rawValue);
+        }
+        if (rawValue.startsWith('var(')) {
+            const reg = /\((.|\n)[^\)]*\)/;
+            const match = rawValue.match(reg);
+            if (match) {
+                let variableName = match[0];
+                variableName = variableName.substr(1);
+                variableName = variableName.substring(0, variableName.length - 1);
+                variableName = variableName.trim();
+                const variableRawValue = map[variableName].rawValue;
+                return getVariableValue(variableRawValue);
+            }
+        }
+        const comps = rawValue.split(',');
+        if (comps.length > 2) {
+            const r = parseInt(comps[0]) / 255;
+            const g = parseInt(comps[1]) / 255;
+            const b = parseInt(comps[2]) / 255;
+            const a = comps.length > 3 ? parseFloat(comps[3]) : 1;
+            return { r, b, g, a };
+        }
+        console.error(`Couldn't parse rawValue "${rawValue}"`);
+        return { r: 0, b: 0, g: 0, a: 0 };
+    };
+    var keys = Object.keys(map);
+    keys.forEach(key => {
+        const variable = map[key];
+        variable.color = getVariableValue(variable.rawValue);
+        console.log(`${variable.name} - ${variable.rawValue} - {r:${variable.color.r}, g:${variable.color.g}, b:${variable.color.b}, a:${variable.color.a}}`);
+    });
     // 1 by 1 retrieve value
+    return [];
 };
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
--- code.ts
+++ code.ts
@@ -20,7 +20,46 @@ class CustomPaint implements SolidPaint {
   }
 }
 
-const parseStyles = (content: string) => {
+class Variable {
+  name: string;
+  rawValue: string;
+  color?: RGBA;
+}
+
+const parseRGBAValue = (rawValue: string):RGBA => {
+  const regex = /\([0-9][^\)]*\)/;
+  const match = rawValue.match(regex);
+  if(match){
+    let value = match[0];
+    value = value.substr(1);
+    value = value.substring(0, value.length - 1);
+    const values = value.split(',');
+    const r = parseFloat(values[0].trim())/255;
+    const g = parseFloat(values[1].trim())/255;
+    const b = parseFloat(values[2].trim())/255;
+    const a = parseFloat(values[3].trim());
+    return {
+      r,g,b,a
+    }
+  }
+  console.error(`Couldn't parse RGBA value ${rawValue}`);
+  return {r:0,b:0,g:0,a:0};
+}
+
+const parseHexValue = (rawValue: string):RGBA => {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(rawValue);
+  if(result){
+    const r = parseInt(result[1], 16) / 255;
+    const g = parseInt(result[2], 16) / 255;
+    const b = parseInt(result[3], 16) / 255;
+    const a = 1;
+    return {r,g,b,a};
+  }
+  console.error(`Couldn't parse HEX value ${rawValue}`);
+  return {r:0,b:0,g:0,a:0};
+}
+
+const parseStyles = (content: string):Variable[] => {
   // remove comments
   const commentsReg = /\/\*[\s\S]*?\*\/|\/\/.*/g;
   content = content.replace(commentsReg, '');
@@ -29,12 +68,62 @@ const parseStyles = (content: string) => {
   // remove empty lines
   lines = lines.filter(line => line.trim().length>0);
   console.log(`will parse ${lines.length} lines`);
-  lines.forEach(line => {
-    console.log(line);
-  })
   
-  // save variables with value, overriding
+  // save variables with raw value, overriding
+  let map: {[key: string]: Variable} = {};
+  lines.forEach(line => {
+    line = line.trim();
+    const comp = line.split(':');
+    const name = comp[0].trim();
+    let rawValue = comp[1].trim();
+    // remove ':'
+    rawValue = rawValue.substring(0, rawValue.length - 1);
+    const variable = new Variable();
+    variable.name = name;
+    variable.rawValue = rawValue;
+    map[name] = variable;
+  });
+
+  const getVariableValue = (rawValue: string): RGBA => {
+    if(rawValue.startsWith('rgba(')){
+      return parseRGBAValue(rawValue);
+    }
+    if(rawValue.startsWith('#')){
+      return parseHexValue(rawValue);
+    }
+    if(rawValue.startsWith('var(')){
+      const reg = /\((.|\n)[^\)]*\)/;
+      const match = rawValue.match(reg);
+      if(match){
+        let variableName = match[0];
+        variableName = variableName.substr(1);
+        variableName = variableName.substring(0, variableName.length - 1);
+        variableName = variableName.trim();
+        const variableRawValue = map[variableName].rawValue;
+        return getVariableValue(variableRawValue);
+      }
+    }
+    const comps = rawValue.split(',');
+    if(comps.length>2){
+      const r = parseInt(comps[0])/255;
+      const g = parseInt(comps[1])/255;
+      const b = parseInt(comps[2])/255;
+      const a = comps.length>3 ? parseFloat(comps[3]) : 1;
+      return {r,b,g,a};
+    }
+    console.error(`Couldn't parse rawValue "${rawValue}"`);
+    return {r:0,b:0,g:0,a:0};
+  }
+
+  var keys = Object.keys(map);
+  keys.forEach( key => {
+    const variable = map[key];
+    variable.color = getVariableValue(variable.rawValue);
+    console.log(`${variable.name} - ${variable.rawValue} - {r:${variable.color.r}, g:${variable.color.g}, b:${variable.color.b}, a:${variable.color.a}}`);
+  });
+
   // 1 by 1 retrieve value
+  return [];
 }
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this

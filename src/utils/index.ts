6f20b1df707585f1095f85b6c7b5025b2a854d6a
DESKTOP-87F2V2L\edokoa <jadiazes@microsoft.com>
2019-11-18 18:24:14 +0100

    Added 'human sorting' to have sane sorting of variable names when numbers are involved.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+import Token from '../code/model/Token';
 export const getTextWithinBounds = (text: string, startBound: string, endBound: string): string => {
   const start = text.indexOf(startBound);
   const end = text.indexOf(endBound);
@@ -34,4 +35,27 @@ export const parseHexValue = (rawValue: string): RGBA => {
   }
   console.error(`Couldn't parse HEX value ${rawValue}`);
   return { r: 0, b: 0, g: 0, a: 0 };
+};
+
+//humanSort function by mrDoktar
+//Figma doesn't seem to support localeCompare due to sandbox limitations(?)
+//If it worked, we'd be able to substitute this function with localCompare.
+export const humanSort =(tA:Token, tB:Token) => {
+  let a=tA.name;
+  let b=tB.name;
+
+  let aa = a.split(/(\d+)/);
+  let bb = b.split(/(\d+)/);
+
+  for(var x = 0; x < Math.max(aa.length, bb.length); x++) {
+    if(aa[x] != bb[x]) {
+      var cmp1 = (isNaN(parseInt(aa[x],10)))? aa[x] : parseInt(aa[x],10);
+      var cmp2 = (isNaN(parseInt(bb[x],10)))? bb[x] : parseInt(bb[x],10);
+      if(cmp1 == undefined || cmp2 == undefined)
+        return aa.length - bb.length;
+      else
+        return (cmp1 < cmp2) ? -1 : 1;
+    }
+  }
+  return 0;
 };
